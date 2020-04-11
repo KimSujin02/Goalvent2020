c56f1dce92842ec535d13fca869e77efbf89da82
KimSujin02 <s2018w28@e-mirim.hs.kr>
2020-04-11 11:50:17 +0900

    like 인터페이스 수정

--- src/goalvent_interface/like.java
+++ src/goalvent_interface/like.java
@@ -6,10 +6,12 @@ public interface like {
 	// 게시물에 좋아요 추가
 	// 게시물에 좋아요 취소
 	// 내가 이 게시물에 좋아요를 했는지 안했는지 검사
+	// 해당 게시물을 좋아하는 사람들 리스트
 	// 내가 좋아요 누른 게시물들의 넘버
 
-	public int boardLike(int num, String id); // 게시물의 번호와 좋아요를 누른 아이디 그리고 게시물 like의 컬럼의 카운트 수도 추가해야한다. 삭제는 반대로 -1
-	public int deleteLike(int num, String id);
-	public int confirmLike(int num, String id);
-	public ArrayList<Integer> myLike(String id);
+	public int boardLike(int num, String liker, String date); // 게시물의 번호와 좋아요를 누른 아이디 그리고 게시물 like의 컬럼의 카운트 수도 추가해야한다. 삭제는 반대로 -1
+	public int deleteLike(int num, String liker);
+	public int confirmLike(int num, String liker);
+	public ArrayList<String> likers(int num);
+	public ArrayList<Integer> myLike(String liker);
 }
