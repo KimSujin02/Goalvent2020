d1dca084ae1fead3ca4d865d99ce8aa6edd47813
KimSujin02 <s2018w28@e-mirim.hs.kr>
2020-04-12 16:31:53 +0900

    follow 인터페이스 수정

--- src/goalvent_interface/Follow.java
+++ src/goalvent_interface/Follow.java
@@ -7,13 +7,9 @@ public interface Follow {
 	//팔로우취소하기
 	//나를 팔로우한 사람들
 	//내가 팔로우한 사람들
-	//내 팔로워 수
-	//내 팔로우 수
-	//나를 팔로우한 사람인가?
 	
 	public int follow(String id, String follow);
 	public int cancleFollow(String id, String follow);
 	public ArrayList<String> follower(String id);
 	public ArrayList<String> following(String id);
-	public int confirmFollow(String myId, String otherId);
 }
