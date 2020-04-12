5732d57b924e315b9ecc9e3053207d879814248f
KimSujin02 <s2018w28@e-mirim.hs.kr>
2020-04-12 15:58:17 +0900

    내가 팔로우 하는 사람들 리스트기능 구현

--- src/goalvent/FollowDAO.java
+++ src/goalvent/FollowDAO.java
@@ -1,11 +1,22 @@
 package goalvent;
 
-import java.util.Vector;
+import java.sql.*;
+import java.util.*;
 
 import goalvent_interface.Follow;
 
 public class FollowDAO implements Follow {
-
+	Connection conn;
+	PreparedStatement pstmt;
+	String sql;
+	ResultSet rs;
+	
+	DBConnectionMgr dbcp;
+	
+	public FollowDAO() { //»ý¼ºÀÚ 
+		dbcp = DBConnectionMgr.getInstance();
+	}
+	
 	@Override
 	public int follow(String id, String follow) {
 		// TODO Auto-generated method stub
@@ -19,15 +30,28 @@ public class FollowDAO implements Follow {
 	}
 
 	@Override
-	public Vector<String> follower(String id) {
+	public ArrayList<String> follower(String id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 	@Override
-	public Vector<String> following(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<String> following(String id) {
+		ArrayList<String> followList = new ArrayList<String>();
+		sql = "SELECT following FROM follow WHERE id=?";
+		try {
+			conn = dbcp.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				followList.add(rs.getString("following"));
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return followList;
 	}
 
 	@Override
