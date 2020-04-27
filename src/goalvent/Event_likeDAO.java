c2193f0a89a66e2b6f6f4d853b73adc27a055216
KimSujin02 <s2018w28@e-mirim.hs.kr>
2020-04-28 05:46:17 +0900

    이벤트 좋아요 기능 완성

--- src/goalvent/Event_likeDAO.java
+++ src/goalvent/Event_likeDAO.java
@@ -23,7 +23,7 @@ public class Event_likeDAO implements like {
 	@Override
 	public int boardLike(int num, String liker) {
 		int result = 1; // 성공 1 실패 0
-		sql = "INSERT INTO board_like VALUES(?, ?, date_format(NOW(), '%Y%m%d%H%i%s'))";
+		sql = "INSERT INTO event_like VALUES(?, ?, date_format(NOW(), '%Y%m%d%H%i%s'))";
 		try {
 			conn = dbcp.getConnection();
 			pstmt = conn.prepareStatement(sql);
@@ -45,26 +45,93 @@ public class Event_likeDAO implements like {
 
 	@Override
 	public int deleteLike(int num, String liker) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 1; // 성공 1 실패 0
+		sql = "DELETE FROM event_like WHERE num=?, liker=?";
+		try {
+			conn = dbcp.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.setString(2, liker);
+			pstmt.executeUpdate();
+			sql = "UPDATE event SET likecount=likecount-1 WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("좋아요 삭제 실패");
+			result = 0;
+		}
+		return result;
 	}
 
 	@Override
 	public int confirmLike(int num, String liker) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 1; // 좋아요를 눌렀으면 1 아니면 0 정보 가져오기 실패하면 18
+		sql = "SELECT * FROM event_like WHERE num=?, liker=?";
+		try {
+			conn = dbcp.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.setString(2, liker);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = 1;
+			} else {
+				result = 0;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("사용자가 해당게시물에 좋아요를 눌렀는지 확인하지 못함.");
+			result = 18;
+		}
+		return result;
 	}
 
 	@Override
 	public ArrayList<String> likers(int num) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> liker = new ArrayList<String>();
+		String id = "";
+		sql = "SELECT liker FROM event_like WHERE num=?";
+		try {
+			conn = dbcp.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				id = rs.getString("liker");
+				liker.add(id);
+			} else {
+				System.out.println("좋아요를 누른 게시물의 넘버를 가져오지 못함.");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("좋아요를 누른 게시물의 넘버를 가져오지 못함.");
+		}
+		return liker;
 	}
 
 	@Override
 	public ArrayList<Integer> myLike(String liker) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Integer> likelist = new ArrayList<Integer>();
+		int num;
+		sql = "SELECT num FROM event_like WHERE liker=? ORDER BY date DESC";
+		try {
+			conn = dbcp.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, liker);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				num = rs.getInt("num");
+				likelist.add(num);
+			} else {
+				System.out.println("사용자가 좋아요를 누른 게시물의 넘버를 가져오지 못함.");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("사용자가 좋아요를 누른 게시물의 넘버를 가져오지 못함.");
+		}
+		return likelist;
 	}
 
 }
